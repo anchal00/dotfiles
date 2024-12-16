207c05c404efd664bd7dbac12b5170dd1f686cc4
anchal00 <anchal82199@gmail.com>
2024-12-16 18:49:31 +0530

    Add more remaps

--- nvim/.config/nvim/lua/personal/keymaps.lua
+++ nvim/.config/nvim/lua/personal/keymaps.lua
@@ -1,6 +1,15 @@
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>bb", vim.cmd.Ex)
+
 vim.keymap.set("i", "jk", "<Esc>", opts)
 
-print("Set keymaps successfully")
+-- Pane Navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts) -- Move Left
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts) -- Move Down
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts) -- Move Up
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts) -- Move Right
+
+-- Window Management
+vim.keymap.set("n", "<leader>sv", vim.cmd.vsplit, opts)
+vim.keymap.set("n", "<leader>sh", vim.cmd.split, opts)
