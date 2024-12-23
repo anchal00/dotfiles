0cf02e855ec558808bc7fa549ee3882b5522436b
anchal00 <anchal82199@gmail.com>
2024-12-23 14:25:09 +0530

    Kitty config

--- kitty/.config/kitty/kitty-scrollback-setup.sh
+++ kitty/.config/kitty/kitty-scrollback-setup.sh
@@ -6,8 +6,7 @@ allow_remote_control socket-only
 shell_integration enabled
 listen_on unix:@kitty
 ' >> $PWD/kitty/.config/kitty/kitty.conf
-
-nvim --headless +'KittyScrollbackGenerateKittens' 2>> $PWD/kitty/.config/kitty/kitty.conf
-# tr -d '\r' < $PWD/kitty/.config/kitty/kitty.conf > $PWD/kitty/.config/kitty/kitty.conf
-
-echo -e '\nDone ! As a final setup, completely close and reopen Kitty and check the health of kitty-scrollback.nvim\n'
+echo -e '\n ------------COPY AND PASTE THE FOLLOWING INTO YOUR kitty.conf--------------------'
+nvim --headless +'KittyScrollbackGenerateKittens'
+echo -e '\n ---------------------------------------------------------------------------------'
+echo -e 'Done ! finally, Completely close and reopen Kitty and check the health of kitty-scrollback.nvim\n'
